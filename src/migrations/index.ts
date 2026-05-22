0065a91b23d72c8e50df91f9bad3b5e15729f182
Razvan Bielz <razvan.bielz@gmail.com>
2026-05-22 20:57:33 +0300

    fix: migrations file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20260522_140221_product_type_and_tags from './20260522_140221_product_type_and_tags';
-
-export const migrations = [
-  {
-    up: migration_20260522_140221_product_type_and_tags.up,
-    down: migration_20260522_140221_product_type_and_tags.down,
-    name: '20260522_140221_product_type_and_tags'
-  },
-];
+export const migrations = [];
